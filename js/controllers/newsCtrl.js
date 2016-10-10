585b1a11ed6852892138bf7a3dc35874c78a7088
Thai Tran <Thai.Tran5656@gmail.com>
2016-10-10 13:31:26 -0700

    nav centered

--- js/controllers/newsCtrl.js
+++ js/controllers/newsCtrl.js
@@ -1,8 +1,41 @@
 angular.module("ufcApp").controller('newsCtrl', function($scope, mainService) {
     mainService.getNews().then(function(response) {
+        for (var i = 0; i < response.length; i++){
+          response[i].checkbox = '';
+        }
         $scope.getNews = response;
         console.log(response);
-      
+
     })
+
+          $scope.checkedItems = function(index) {
+            for (var i = 0; i < $scope.getNews.length; i++) {
+              if ($scope.getNews[i].title === index) {
+                $scope.getNews[i].checkbox = !$scope.getNews[i].checkbox
+              }
+            }
+          }
+
+        // $scope.myAppObjects = {
+        //     id: 1,
+        //     checkbox: true
+        // }
+        //
+        // $scope.checkedItems = function() {
+        //     var checkedItems = [];
+        //     angular.forEach($scope.myAppObjects, function(appObj, arrayIndex) {
+        //             angular.forEach(appObj, function(cb, key) {
+        //                 if (key.substring(0, 2) == "cb" && cb) {
+        //                     checkedItems.push(appObj.id + '-' + key)
+        //                 }
+        //             })
+        //             return checkedItems ;
+        //         })
+        //     }
+
+
+
+
+
 
 })
